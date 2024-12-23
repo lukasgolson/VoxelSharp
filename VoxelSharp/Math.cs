89915f040008539eda1b1433569884e6936bc4f4
Lukas G. Olson <olson@student.ubc.ca>
2024-12-23 13:33:43 -0800

    Added the Euclidean modulo operation

---
+++ VoxelSharp/Math.cs
@@ -0,0 +1,15 @@
+﻿namespace VoxelSharp;
+
+public static class Math
+{
+    /// <summary>
+    /// Computes the Euclidean modulo operation, ensuring a non-negative result.
+    /// </summary>
+    /// <param name="value">The value to be divided.</param>
+    /// <param name="mod">The divisor.</param>
+    /// <returns>The remainder of the division, adjusted to be non-negative.</returns>
+    public static int EModulo(int value, int mod)
+    {
+        return (value % mod + mod) % mod;
+    }
+}
