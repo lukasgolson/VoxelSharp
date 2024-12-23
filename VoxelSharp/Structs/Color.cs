3d2e1b06cf53998a3f0b12869810b44c6bd3f61c
Lukas G. Olson <olson@student.ubc.ca>
2024-12-22 19:36:50 -0800

    Updated Color.cs

--- VoxelSharp/Structs/Color.cs
+++ VoxelSharp/Structs/Color.cs
@@ -1,9 +1,20 @@
 ﻿namespace VoxelSharp.Structs;
 
-public readonly struct Color(float r, float g, float b, float a)
+public readonly struct Color(byte r, byte g, byte b, byte a)
 {
-    float R { get; } = r;
-    float G { get; } = g;
-    float B { get; } = b;
-    float A { get; } = a;
+    public byte R { get; } = r;
+    public byte G { get; } = g;
+    public byte B { get; } = b;
+    public byte A { get; } = a;
+
+
+    public byte this[int index] => index switch
+    {
+        0 => R,
+        1 => G,
+        2 => B,
+        3 => A,
+        _ => throw new IndexOutOfRangeException("Invalid index")
+    };
+    
 }
