acf72221bb00c412ff615c7f97e6ed97f0408cbd
Lukas G. Olson <olson@student.ubc.ca>
2024-12-23 19:38:50 -0800

    Added IEquatable and default static colors to color.cs

--- VoxelSharp/Structs/Color.cs
+++ VoxelSharp/Structs/Color.cs
@@ -1,6 +1,6 @@
 ﻿namespace VoxelSharp.Structs;
 
-public readonly struct Color(byte r, byte g, byte b, byte a)
+public readonly struct Color(byte r, byte g, byte b, byte a) : IEquatable<Color>
 {
     public byte R { get; } = r;
     public byte G { get; } = g;
@@ -16,5 +16,32 @@ public readonly struct Color(byte r, byte g, byte b, byte a)
         3 => A,
         _ => throw new IndexOutOfRangeException("Invalid index")
     };
+
+    public override bool Equals(object? obj) => obj is Color other &&
+                                                R == other.R && G == other.G && B == other.B && A == other.A;
+
+    public override int GetHashCode() => HashCode.Combine(R, G, B, A);
+
+    public static bool operator ==(Color left, Color right) => left.Equals(right);
+
+    public static bool operator !=(Color left, Color right) => !left.Equals(right);
+
+
+    public bool Equals(Color other)
+    {
+        return R == other.R && G == other.G && B == other.B && A == other.A;
+    }
+
+    public static Color FromGrayscale(byte intensity)
+    {
+        return new Color(intensity, intensity, intensity, 255);
+    }
+
+    public static Color Red { get; } = new(255, 0, 0, 255);
+    public static Color Green { get; } = new(0, 255, 0, 255);
+    public static Color Blue { get; } = new(0, 0, 255, 255);
+    public static Color White { get; } = new(255, 255, 255, 255);
+    public static Color Black { get; } = new(0, 0, 0, 255);
     
+    public static Color Transparent { get; } = new(0, 0, 0, 0);
 }
